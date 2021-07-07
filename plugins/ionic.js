3a22fe56efea5c400557bc5088106b22d34ab59e
Colin Brayton <colin.brayton17@gmail.com>
2021-07-07 10:51:41 -0300

    Initial commit

---
+++ plugins/ionic.js
@@ -0,0 +1,8 @@
+import Vue from 'vue'
+// add a direct link to @ionic/core
+import { defineCustomElements as Ionic } from '@ionic/core/loader'
+import { defineCustomElements as Pwa } from '@ionic/pwa-elements/loader'
+
+Vue.use(Ionic)
+Vue.use(Pwa)
+Vue.config.ignoredElements = [/^ion-/]
